a1937910ab4d4063211ff956b28115c844f12574
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-08 18:40:33 +0530

    This a swapping without a temporary variable

---
+++ program10.py
@@ -0,0 +1,19 @@
+"""
+What a  Python Program to Swap Two Variables Without Temp Variable.
+
+#swapping without a temporary variable 
+
+
+"""
+
+a = 5
+b = 10
+#swapping without a temporary variable 
+
+a,b = b,a 
+
+print("After Swapping .")
+print("a =",a)
+print("b =",b)
+
+# end of the program 
