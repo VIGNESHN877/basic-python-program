22fc81e0db83c6f444f14cb81cc34f9554c3c8d7
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-11 21:07:44 +0530

    text

--- program19.py
+++ program19.py
@@ -4,5 +4,14 @@ Write a Python Program to Check Armstrong Number ?
 
 Armstrong Number :
 
-it is a number that is equal to the sum 
+it is a number that is equal to the sum of its own digits,
+each raised to a power equal to the number of digits in the number 
+
+
+for a example . lets consider the number 153.
+
+. it has three digits (1,5 and 3).
+. if we calculate 1^3 + 5^3 + 3^3 . we get 1+153+27 , Which is equal to 153.
+
+
 """
