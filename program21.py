db4d5c4ec5bc8e6c2b78b2f5b346fee8bddcaa95
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-12 15:37:38 +0530

    program 21

---
+++ program21.py
@@ -0,0 +1,14 @@
+"""
+Write a Python program to Find the sum of Natural Numbers .
+
+
+Natural numbers are a set of positive integers that are used to count and order objects .
+they are the numbers that typically start 1 and continue indefinitely, including all the 
+whole numbers greater that 0 , in mathemathical notation, the set of natural numbers is often
+denoted as "N" and can be axpressed as :
+                       N = 1,2,3,4,5,6,7,8
+
+
+
+
+"""
