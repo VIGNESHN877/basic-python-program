db9ed9eb9426d3bb0ea430bc17b621fd9e8f8468
VIGNESHN877 <vigneshn877@gmail.com>
2026-06-12 16:42:03 +0530

    python Program to find LCM.

--- program22.py
+++ program22.py
@@ -1,5 +1,14 @@
 """
 write a python Program to find LCM.
 
+LCM OR Least Common Multiple, is the smallest number that is a multiple of two or more numbers.
+
+formula:
+
+for two numbers a and b, the LCM can be calculated using the following formula:
+
+LCM(a, b) = (a * b) / GCD(a, b)
+
+for more than two numbers, the LCM can be calculated iteratively.
 
 """
